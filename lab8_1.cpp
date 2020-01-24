f863f172ef1d505eaf31d2c69986d9a4034471fc
Mobmab_Lee <59276838+mobmabpp@users.noreply.github.com>
2020-01-24 19:03:44 +0700

    Update lab8_1.cpp

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,30 +1,50 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip>
 using namespace std;
 
-int main(){	
+int main(){
+    double loan,rate,pay;
+    int i=1;
+    cout << "Enter initial loan : ";
+    cin >> loan;
+    cout << "Enter interest rate per year (%) : ";
+    cin >> rate;
+    cout << "Enter amount you can pay per year : ";
+    cin >> pay;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+    cout << setw(13) << left << "EndOfYear#";
+    cout << setw(13) << left << "PrevBalance";
+    cout << setw(13) << left << "Interest";
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
+    
+    for(;;i++)
+    {
+        double interest = rate/100*loan;
+        double total = interest+loan;
+        if (pay>total)
+        {
+            pay=total;
+            
+        }
+        double newb = total-pay;
+        cout << fixed << setprecision(2);
+        cout << setw(13) << left << i;
+        cout << setw(13) << left << loan;
+        cout << setw(13) << left << interest;
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << pay;
+        cout << setw(13) << left << newb;
+        loan=newb;
+        cout << "\n";
+    if (newb==0)
+        {
+            break;
+        }
+        
+    }
+
+    return 0;
 }
